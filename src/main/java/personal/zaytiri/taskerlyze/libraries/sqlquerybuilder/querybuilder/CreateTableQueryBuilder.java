8aad7f680600b613ca879c7a0fa984e962e0e62d
zay <64491586+zaytiri@users.noreply.github.com>
2023-05-06 20:20:47 +0100

    added create table clause in query builder

---
+++ src/main/java/personal/zaytiri/taskerlyze/libraries/sqlquerybuilder/querybuilder/CreateTableQueryBuilder.java
@@ -0,0 +1,37 @@
+package personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.querybuilder;
+
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.Column;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.Table;
+
+import java.sql.Connection;
+
+public class CreateTableQueryBuilder extends QueryBuilder{
+
+
+    public CreateTableQueryBuilder(Connection connection) {
+        super(connection);
+    }
+
+    public void create(Table table){
+        query.append("create table ");
+        query.append(table.getName());
+        query.append(" ( ");
+
+        String primaryKey = "";
+
+        for (Column col : table.getColumns()) {
+            query.append(col.getName()).append(" ");
+            query.append(col.getType()).append(" ");
+
+            if(!col.getDefaultValue(String.class).equalsIgnoreCase("null")){
+                query.append("not null, ");
+            }
+
+            if(col.getIsPrimaryKey()){
+                primaryKey = col.getName();
+            }
+        }
+
+        query.append("primary key ( ").append(primaryKey).append(" ) )");
+    }
+}
