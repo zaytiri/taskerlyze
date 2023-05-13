020c7eeb028636ac01cf5fa128ff4b3765791aa5
zay <64491586+zaytiri@users.noreply.github.com>
2023-05-13 20:03:34 +0100

    implement tests for CreateTableQueryBuilder
    added CreateTableQueryBuilder tests; added a database helper for tests, containing common used methods throughout all tests;

---
+++ src/test/java/sqlquerybuilder/CreateTableQueryBuilderTests.java
@@ -0,0 +1,62 @@
+package sqlquerybuilder;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.querybuilder.query.CreateTableQueryBuilder;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.querybuilder.schema.Table;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.response.Response;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CreateTableQueryBuilderTests {
+    private final static Logger LOGGER = Logger.getLogger(CreateTableQueryBuilderTests.class.getName());
+
+    @BeforeAll
+    public static void createDatabaseSchema() {
+        DatabaseTestHelper.createDatabaseSchema();
+    }
+
+    @AfterAll
+    public static void deleteDatabase() {
+        DatabaseTestHelper.deleteDatabase();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"tasks", "people"})
+    void Should_CreateTableInDatabase(String tableName) {
+        Connection connection = null;
+        try {
+            // arrange
+            connection = DriverManager.getConnection("jdbc:sqlite:" + DatabaseTestHelper.getDbConnectionPath());
+            Table table = DatabaseTestHelper.getSchema().getTable(tableName);
+
+            // act
+            CreateTableQueryBuilder query = new CreateTableQueryBuilder(connection);
+            query.setCloseConnection(false);
+            query.create(table);
+            Response response = query.execute();
+
+            String tableExists = "SELECT name FROM sqlite_master WHERE type='table' AND name='" + tableName + "'";
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(tableExists);
+            rs.next();
+
+            String result = rs.getString("name");
+
+            statement.close();
+            connection.close();
+
+            // assert
+            Assertions.assertTrue(response.isSuccess());
+            Assertions.assertEquals(tableName, result);
+
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Should_CreateTableInDatabase: " + e.getMessage(), e);
+        }
+    }
+}
---
+++ src/test/java/sqlquerybuilder/DatabaseTestHelper.java
@@ -0,0 +1,174 @@
+package sqlquerybuilder;
+
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.querybuilder.schema.Column;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.querybuilder.schema.Database;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.querybuilder.schema.Table;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DatabaseTestHelper {
+    private final static Logger LOGGER = Logger.getLogger(DatabaseTestHelper.class.getName());
+    private static Database schema;
+
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static void createDatabaseSchema() {
+        // table tasks
+        Table tasks = new Table();
+        tasks.setName("tasks");
+
+        Column id = new Column("id", "integer", "1", "true", tasks.getName());
+        Column name = new Column("name", "varchar(255)", "null", "false", tasks.getName());
+        List<Column> tasksColumns = new ArrayList<>();
+        tasksColumns.add(id);
+        tasksColumns.add(name);
+
+        tasks.setColumns(tasksColumns);
+
+        // table people
+        Table people = new Table();
+        people.setName("people");
+
+        id = new Column("id", "integer", "1", "true", people.getName());
+        name = new Column("name", "varchar(255)", "null", "false", people.getName());
+        List<Column> peopleColumns = new ArrayList<>();
+        peopleColumns.add(id);
+        peopleColumns.add(name);
+
+        people.setColumns(peopleColumns);
+
+        // intermediate table tasks_people
+        Table tasksPeople = new Table();
+        tasksPeople.setName("tasks_people");
+
+        id = new Column("id", "integer", "1", "true", tasksPeople.getName());
+        Column tasksId = new Column("tasks_id", "integer", "1", "false", tasksPeople.getName());
+        Column peopleId = new Column("people_id", "integer", "1", "false", tasksPeople.getName());
+        List<Column> tasksPeopleColumns = new ArrayList<>();
+        tasksPeopleColumns.add(id);
+        tasksPeopleColumns.add(tasksId);
+        tasksPeopleColumns.add(peopleId);
+
+        tasksPeople.setColumns(tasksPeopleColumns);
+
+        // add tables to database
+        List<Table> tables = new ArrayList<>();
+        tables.add(tasks);
+        tables.add(people);
+        tables.add(tasksPeople);
+
+        schema = new Database("test_db", tables);
+    }
+
+    public static void createTables() {
+        Connection connection = null;
+        try {
+            // create a database connection
+            connection = DriverManager.getConnection("jdbc:sqlite:" + getDbConnectionPath());
+
+            for (Table table : schema.getTables()) {
+                String query = createTableQuery(table);
+                Statement statement = connection.createStatement();
+
+                statement.executeUpdate("drop table if exists " + table.getName());
+
+                statement.executeUpdate(query);
+                statement.close();
+            }
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "createTables: " + e.getMessage(), e);
+        } finally {
+            closeConnection(connection);
+        }
+    }
+
+    public static void deleteDatabase() {
+        try {
+            Files.delete(Path.of(getDbConnectionPath()));
+        } catch (Exception e) {
+            // if any error occurs
+            LOGGER.log(Level.SEVERE, "deleteDatabase: " + e.getMessage(), e);
+        }
+    }
+
+    public static String getDbConnectionPath() {
+        String currentDirectory = Path.of("").toAbsolutePath().toString();
+        String databaseName = "sqlquerybuilder_test.db";
+        return currentDirectory + "\\src\\test\\resources\\sqlquerybuilder\\" + databaseName;
+    }
+
+    public static Database getSchema() {
+        return schema;
+    }
+
+    public static void insertMockData() {
+        Connection connection = null;
+        try {
+            // create a database connection
+            connection = DriverManager.getConnection("jdbc:sqlite:" + getDbConnectionPath());
+
+            List<String> queries = new ArrayList<>() {
+                {
+                    add("insert into people (id, name) values (1, 'cat')");
+                    add("insert into people (id, name) values (2, 'nuno')");
+                    add("insert into people (id, name) values (3, 'pedro')");
+                    add("insert into people (id, name) values (4, 'monica')");
+                    add("insert into tasks (id, name) values (1, 'do something weird')");
+                    add("insert into tasks (id, name) values (2, 'dont do that')");
+                    add("insert into tasks (id, name) values (3, 'do anything spectacular')");
+                    add("insert into tasks_people (id, tasks_id, people_id) values (1, 1, 1)");
+                    add("insert into tasks_people (id, tasks_id, people_id) values (2, 2, 1)");
+                    add("insert into tasks_people (id, tasks_id, people_id) values (3, 3, 4)");
+                }
+            };
+
+            Statement statement = connection.createStatement();
+            for (String query : queries) {
+                statement.executeUpdate(query);
+            }
+            statement.close();
+
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "insertMockData: " + e.getMessage(), e);
+        } finally {
+            closeConnection(connection);
+        }
+    }
+
+    private static String createTableQuery(Table table) {
+        StringBuilder query = new StringBuilder();
+        query.append("create table ").append(table.getName()).append(" ( ");
+        String primaryKey = "";
+        for (Column col : table.getColumns()) {
+            query.append(col.getName()).append(" ");
+            query.append(col.getType());
+            if (!col.getDefaultValue(String.class).equalsIgnoreCase("null")) {
+                query.append(" not null");
+            }
+            if (col.getIsPrimaryKey()) {
+                primaryKey = col.getName();
+            }
+            query.append(", ");
+        }
+        query.append("primary key ( ").append(primaryKey).append(" ) )");
+
+        return query.toString();
+    }
+}
