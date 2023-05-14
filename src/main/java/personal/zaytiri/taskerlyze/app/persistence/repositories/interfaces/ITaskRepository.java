4dcc71db136033dc648c0c2064f61eae053f38a5
zay <64491586+zaytiri@users.noreply.github.com>
2023-05-14 19:39:05 +0100

    added interface for task repository

---
+++ src/main/java/personal/zaytiri/taskerlyze/app/persistence/repositories/interfaces/ITaskRepository.java
@@ -0,0 +1,11 @@
+package personal.zaytiri.taskerlyze.app.persistence.repositories.interfaces;
+
+import personal.zaytiri.taskerlyze.app.api.domain.Task;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.response.Response;
+
+public interface ITaskRepository extends IRepository<Task> {
+
+    Response done();
+
+    Response undone();
+}
