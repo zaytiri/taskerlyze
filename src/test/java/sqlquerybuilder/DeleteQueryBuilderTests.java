bb35ad37d200f5085cc069aa4e7171273bd3ce32
zay <64491586+zaytiri@users.noreply.github.com>
2023-05-14 19:37:16 +0100

    implement tests for update & delete query builders

---
+++ src/test/java/sqlquerybuilder/DeleteQueryBuilderTests.java
@@ -0,0 +1,96 @@
+package sqlquerybuilder;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.querybuilder.query.DeleteQueryBuilder;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.querybuilder.query.Operators;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.querybuilder.schema.Column;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.querybuilder.schema.Table;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.response.Response;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DeleteQueryBuilderTests {
+    private final static Logger LOGGER = Logger.getLogger(DeleteQueryBuilderTests.class.getName());
+
+    @BeforeAll
+    public static void createDatabaseSchema() {
+        DatabaseTestHelper.createDatabaseSchema();
+        DatabaseTestHelper.createTables();
+        DatabaseTestHelper.insertMockData();
+    }
+
+    @AfterAll
+    public static void deleteDatabase() {
+        DatabaseTestHelper.deleteDatabase();
+    }
+
+    @Test
+    void Should_DeleteAllInTablePeople() {
+        Connection connection = null;
+        try {
+            // arrange
+            connection = DriverManager.getConnection("jdbc:sqlite:" + DatabaseTestHelper.getDbConnectionPath());
+            Table people = DatabaseTestHelper.getSchema().getTable("people");
+            int rowCountBefore = DatabaseTestHelper.getRowCount(connection, "select * from people");
+
+            // act
+            DeleteQueryBuilder query = new DeleteQueryBuilder(connection);
+            query.setCloseConnection(false);
+            query.deleteFrom(people);
+            Response response = query.execute();
+            int rowCountAfter = DatabaseTestHelper.getRowCount(connection, "select * from people");
+            LOGGER.log(Level.INFO, response.getQueryExecuted());
+
+            // assert
+            Assertions.assertTrue(response.isSuccess());
+            Assertions.assertEquals(0, response.getResult().size());
+
+            Assertions.assertEquals(6, rowCountBefore);
+            Assertions.assertEquals(0, rowCountAfter);
+
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Should_DeleteAllInTablePeople: " + e.getMessage(), e);
+        } finally {
+            DatabaseTestHelper.closeConnection(connection);
+        }
+    }
+
+    @Test
+    void Should_DeleteRowWhereIdEquals2InTasksTable() {
+        Connection connection = null;
+        try {
+            // arrange
+            connection = DriverManager.getConnection("jdbc:sqlite:" + DatabaseTestHelper.getDbConnectionPath());
+            Table tasks = DatabaseTestHelper.getSchema().getTable("tasks");
+            Column id = tasks.getColumn("id");
+            int rowCountBefore = DatabaseTestHelper.getRowCount(connection, "select * from tasks where id=2");
+
+            // act
+            DeleteQueryBuilder query = new DeleteQueryBuilder(connection);
+            query.setCloseConnection(false);
+            query.deleteFrom(tasks).where(id, Operators.EQUALS, 2);
+            Response response = query.execute();
+            int rowCountAfter = DatabaseTestHelper.getRowCount(connection, "select * from tasks where id=2");
+            LOGGER.log(Level.INFO, response.getQueryExecuted());
+
+            // assert
+            Assertions.assertTrue(response.isSuccess());
+            Assertions.assertEquals(0, response.getResult().size());
+
+            Assertions.assertEquals(1, rowCountBefore);
+            Assertions.assertEquals(0, rowCountAfter);
+
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Should_DeleteRowWhereIdEquals2InTasksTable: " + e.getMessage(), e);
+        } finally {
+            DatabaseTestHelper.closeConnection(connection);
+        }
+    }
+}
---
+++ src/test/java/sqlquerybuilder/UpdateQueryBuilderTests.java
@@ -0,0 +1,146 @@
+package sqlquerybuilder;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.querybuilder.query.Operators;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.querybuilder.query.UpdateQueryBuilder;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.querybuilder.schema.Column;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.querybuilder.schema.Table;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.response.Response;
+
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class UpdateQueryBuilderTests {
+    private final static Logger LOGGER = Logger.getLogger(InsertQueryBuilderTests.class.getName());
+
+    @BeforeAll
+    public static void createDatabaseSchema() {
+        DatabaseTestHelper.createDatabaseSchema();
+        DatabaseTestHelper.createTables();
+        DatabaseTestHelper.insertMockData();
+    }
+
+    @AfterAll
+    public static void deleteDatabase() {
+        DatabaseTestHelper.deleteDatabase();
+    }
+
+    @Test
+    void Should_UpdateRowWithId2UsingMultipleSetMethodsFromTablePeople() {
+        Connection connection = null;
+        try {
+            // arrange
+            connection = DriverManager.getConnection("jdbc:sqlite:" + DatabaseTestHelper.getDbConnectionPath());
+            Table people = DatabaseTestHelper.getSchema().getTable("people");
+
+            // act
+            UpdateQueryBuilder query = new UpdateQueryBuilder(connection);
+            query.setCloseConnection(false);
+
+            query.update(people)
+                    .set(people.getColumn("name"), "andrew")
+                    .set(people.getColumn("id"), 88)
+                    .where(people.getColumn("id"), Operators.EQUALS, 2);
+            Response response = query.execute();
+            LOGGER.log(Level.INFO, response.getQueryExecuted());
+
+            // assert
+            Assertions.assertTrue(response.isSuccess());
+            Assertions.assertEquals(0, response.getResult().size());
+
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from people where id=88");
+            rs.next();
+
+            Assertions.assertEquals("andrew", rs.getString("name"));
+            Assertions.assertNotEquals("nuno", rs.getString("name"));
+
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Should_UpdateRowWithId2UsingMultipleSetMethodsFromTablePeople: " + e.getMessage(), e);
+        } finally {
+            DatabaseTestHelper.closeConnection(connection);
+        }
+    }
+
+    @Test
+    void Should_UpdateRowWithId2UsingSetMethodFromTablePeople() {
+        Connection connection = null;
+        try {
+            // arrange
+            connection = DriverManager.getConnection("jdbc:sqlite:" + DatabaseTestHelper.getDbConnectionPath());
+            Table people = DatabaseTestHelper.getSchema().getTable("people");
+
+            // act
+            UpdateQueryBuilder query = new UpdateQueryBuilder(connection);
+            query.setCloseConnection(false);
+
+            query.update(people)
+                    .set(people.getColumn("name"), "mary")
+                    .where(people.getColumn("id"), Operators.EQUALS, 3);
+            Response response = query.execute();
+            LOGGER.log(Level.INFO, response.getQueryExecuted());
+
+            // assert
+            Assertions.assertTrue(response.isSuccess());
+            Assertions.assertEquals(0, response.getResult().size());
+
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from people where id=3");
+            rs.next();
+
+            Assertions.assertEquals("mary", rs.getString("name"));
+            Assertions.assertNotEquals("pedro", rs.getString("name"));
+
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Should_UpdateRowWithId2UsingSetMethodFromTablePeople: " + e.getMessage(), e);
+        } finally {
+            DatabaseTestHelper.closeConnection(connection);
+        }
+    }
+
+    @Test
+    void Should_UpdateRowWithId4UsingValuesMethodFromTablePeople() {
+        Connection connection = null;
+        try {
+            // arrange
+            connection = DriverManager.getConnection("jdbc:sqlite:" + DatabaseTestHelper.getDbConnectionPath());
+            Table people = DatabaseTestHelper.getSchema().getTable("people");
+
+            // act
+            UpdateQueryBuilder query = new UpdateQueryBuilder(connection);
+            query.setCloseConnection(false);
+
+            Map<Column, Object> updateValues = new HashMap<>() {{
+                put(people.getColumn("name"), "kathy");
+            }};
+
+            query.update(people)
+                    .values(updateValues)
+                    .where(people.getColumn("id"), Operators.EQUALS, 4);
+            Response response = query.execute();
+            LOGGER.log(Level.INFO, response.getQueryExecuted());
+
+            // assert
+            Assertions.assertTrue(response.isSuccess());
+            Assertions.assertEquals(0, response.getResult().size());
+
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from people where id=4");
+            rs.next();
+
+            Assertions.assertEquals("kathy", rs.getString("name"));
+            Assertions.assertNotEquals("monica", rs.getString("name"));
+
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Should_UpdateRowWithId4FromTablePeople: " + e.getMessage(), e);
+        } finally {
+            DatabaseTestHelper.closeConnection(connection);
+        }
+    }
+}
