99bda0e8a17ca652b93efe233001afb8dd71b2a4
zay <64491586+zaytiri@users.noreply.github.com>
2023-05-18 14:27:21 +0100

    add create update get delete methods for task and task controller

---
+++ src/main/java/personal/zaytiri/taskerlyze/app/api/controllers/TaskController.java
@@ -0,0 +1,60 @@
+package personal.zaytiri.taskerlyze.app.api.controllers;
+
+import personal.zaytiri.taskerlyze.app.api.controllers.base.Controller;
+import personal.zaytiri.taskerlyze.app.api.controllers.result.CodeResult;
+import personal.zaytiri.taskerlyze.app.api.controllers.result.MessageResult;
+import personal.zaytiri.taskerlyze.app.api.controllers.result.OperationResult;
+import personal.zaytiri.taskerlyze.app.api.domain.Task;
+
+public class TaskController implements Controller<Task> {
+
+    @Override
+    public OperationResult<Task> createOrUpdate(Task request) {
+        boolean isCreated = request.createOrUpdate();
+
+        MessageResult message = new MessageResult();
+        if (isCreated) {
+            message.setCode(CodeResult.CREATED);
+        } else {
+            message.setCode(CodeResult.NOT_CREATED);
+        }
+
+        return new OperationResult<>(isCreated, message, request);
+    }
+
+    @Override
+    public OperationResult<Task> delete(int id) {
+        Task taskToBeDeleted = Task.getInstance();
+
+        taskToBeDeleted.setId(id);
+
+        boolean isDeleted = taskToBeDeleted.delete();
+
+        MessageResult message = new MessageResult();
+        if (isDeleted) {
+            message.setCode(CodeResult.DELETED);
+        } else {
+            message.setCode(CodeResult.NOT_DELETED);
+        }
+
+        return new OperationResult<>(isDeleted, message, null);
+    }
+
+    @Override
+    public OperationResult<Task> get(int id) {
+        Task task = Task.getInstance();
+
+        task.setId(id);
+
+        boolean isPopulated = task.populate();
+
+        MessageResult message = new MessageResult();
+        if (isPopulated) {
+            message.setCode(CodeResult.FOUND);
+        } else {
+            message.setCode(CodeResult.NOT_FOUND);
+        }
+
+        return new OperationResult<>(isPopulated, message, task);
+    }
+}
--- src/main/java/personal/zaytiri/taskerlyze/app/api/domain/Task.java
+++ src/main/java/personal/zaytiri/taskerlyze/app/api/domain/Task.java
@@ -1,18 +1,58 @@
 package personal.zaytiri.taskerlyze.app.api.domain;
 
-public class Task {
 import personal.zaytiri.taskerlyze.app.dependencyinjection.AppComponent;
 import personal.zaytiri.taskerlyze.app.dependencyinjection.DaggerAppComponent;
+import personal.zaytiri.taskerlyze.app.persistence.repositories.interfaces.ITaskRepository;
+import personal.zaytiri.taskerlyze.libraries.sqlquerybuilder.response.Response;
 
+import javax.inject.Inject;
+import java.util.Map;
+
+public class Task {
     private final ITaskRepository repository;
     private int id;
     private String name;
     private String description;
 
-    public Task() {}
     @Inject
     public Task(ITaskRepository repository) {
         this.repository = repository;
+    }
+
+    public boolean createOrUpdate() {
+        Response response;
+
+        if (!exists()) {
+            response = repository.create(this);
+        } else {
+            response = repository.update(this);
+        }
+
+        return response.isSuccess();
+    }
+
+    public boolean delete() {
+        if (!exists()) {
+            return false;
+        }
+
+        Response response = repository.delete(this);
+        return response.isSuccess();
+    }
+
+    public boolean exists() {
+        Response response = repository.exists(this);
+
+        return !response.getResult().isEmpty();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Task setDescription(String description) {
+        this.description = description;
+        return this;
     }
 
     public int getId() {
@@ -38,12 +78,19 @@ import personal.zaytiri.taskerlyze.app.dependencyinjection.DaggerAppComponent;
         return this;
     }
 
-    public String getDescription() {
-        return description;
-    }
+    public boolean populate() {
+        if (!exists()) {
+            return false;
+        }
 
-    public Task setDescription(String description) {
-        this.description = description;
-        return this;
+        Response response = repository.read(this);
+
+        Map<String, String> result = response.getResult().get(0);
+
+        setId(Integer.parseInt(result.get("id")));
+        setName(result.get("name"));
+        setDescription(result.get("description"));
+
+        return true;
     }
 }
