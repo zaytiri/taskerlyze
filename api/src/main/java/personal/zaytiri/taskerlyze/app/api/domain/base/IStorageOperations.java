3e278d7d8dce597b3361d2964b6eade40f920b50
zay <64491586+zaytiri@users.noreply.github.com>
2023-08-27 17:30:09 +0100

    add IStorageOperations;
    this interface will have all the methods signatures related to database storage for the domain entities

---
+++ api/src/main/java/personal/zaytiri/taskerlyze/app/api/domain/base/IStorageOperations.java
@@ -0,0 +1,15 @@
+package personal.zaytiri.taskerlyze.app.api.domain.base;
+
+import personal.zaytiri.taskerlyze.libraries.pairs.Pair;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IStorageOperations<T> {
+
+    boolean createOrUpdate();
+    boolean delete();
+    boolean exists();
+    T get();
+    List<T> get(Map<String, Pair<String, Object>> filters, Pair<String, String> orderByColumn);
+}
