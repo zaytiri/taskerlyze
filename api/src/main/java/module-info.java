233d7bb40efa0a1fd8c6f234afc8c4e7fda1e79d
zay <64491586+zaytiri@users.noreply.github.com>
2023-08-27 17:50:08 +0100

    add exports to module-info;

--- api/src/main/java/module-info.java
+++ api/src/main/java/module-info.java
@@ -3,4 +3,7 @@ module api.main {
     requires dagger;
     requires org.json;
     exports personal.zaytiri.taskerlyze.app.api.controllers;
+    exports personal.zaytiri.taskerlyze.app.api.domain;
+    exports personal.zaytiri.taskerlyze.app.api.controllers.result;
+    exports personal.zaytiri.taskerlyze.libraries.pairs;
 }
