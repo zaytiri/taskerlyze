b61bfd24edc0fcc9a98ff11a9c682f84b993afa9
zay <64491586+zaytiri@users.noreply.github.com>
2023-08-27 17:38:02 +0100

    update task controller to extend from controller new class;

--- api/src/main/java/personal/zaytiri/taskerlyze/app/api/controllers/TaskController.java
+++ api/src/main/java/personal/zaytiri/taskerlyze/app/api/controllers/TaskController.java
@@ -5,79 +5,10 @@ import personal.zaytiri.taskerlyze.app.api.controllers.result.CodeResult;
 import personal.zaytiri.taskerlyze.app.api.controllers.result.MessageResult;
 import personal.zaytiri.taskerlyze.app.api.controllers.result.OperationResult;
 import personal.zaytiri.taskerlyze.app.api.domain.Task;
-import personal.zaytiri.taskerlyze.libraries.pairs.Pair;
 
-import java.util.List;
-import java.util.Map;
+public class TaskController extends Controller<Task> {
 
-public class TaskController implements Controller<Task> {
-
-    @Override
-    public OperationResult<Task> createOrUpdate(Task request) {
-        boolean isCreated = request.createOrUpdate();
-
-        MessageResult message = new MessageResult();
-        if (isCreated) {
-            message.setCode(CodeResult.CREATED);
-        } else {
-            message.setCode(CodeResult.NOT_CREATED);
-        }
-
-        return new OperationResult<>(isCreated, message, request);
-    }
-
-    @Override
-    public OperationResult<Task> delete(int id) {
-        Task taskToBeDeleted = Task.getInstance();
-
-        taskToBeDeleted.setId(id);
-
-        boolean isDeleted = taskToBeDeleted.delete();
-
-        MessageResult message = new MessageResult();
-        if (isDeleted) {
-            message.setCode(CodeResult.DELETED);
-        } else {
-            message.setCode(CodeResult.NOT_DELETED);
-        }
-
-        return new OperationResult<>(isDeleted, message, null);
-    }
-
-    @Override
-    public OperationResult<Task> get(int id) {
-        Task task = Task.getInstance();
-
-        task.setId(id);
-
-        Task taskPopulated = task.get();
-
-        MessageResult message = new MessageResult();
-        if (taskPopulated != null) {
-            message.setCode(CodeResult.FOUND);
-        } else {
-            message.setCode(CodeResult.NOT_FOUND);
-        }
-
-        return new OperationResult<>(taskPopulated != null, message, taskPopulated);
-    }
-
-    @Override
-    public OperationResult<List<Task>> get(Map<String, Pair<String, Object>> filters, Pair<String, String> orderByColumn) {
-        Task task = Task.getInstance();
-
-        List<Task> tasks = task.get(filters, orderByColumn);
-
-        MessageResult message = new MessageResult();
-        if (!tasks.isEmpty()) {
-            message.setCode(CodeResult.FOUND);
-        } else {
-            message.setCode(CodeResult.NOT_FOUND);
-        }
-
-        return new OperationResult<>(!tasks.isEmpty(), message, tasks);
-    }
-
+    
     /**
      * Sets the status of a task with a specific given id.
      * It will set the done property to the opposite that is already on the database.
@@ -86,7 +17,7 @@ public class TaskController implements Controller<Task> {
      * @return OperationResult<Task>
      */
     public OperationResult<Task> setDone(int id) {
-        Task task = Task.getInstance();
+        Task task = new Task().getInstance();
         task.setId(id);
 
         boolean isTaskUpdated = task.setTaskStatus();
@@ -100,5 +31,15 @@ public class TaskController implements Controller<Task> {
         }
 
         return new OperationResult<>(isTaskUpdated, message, task);
+    }
+
+    @Override
+    protected Task getEntityInstance(int id) {
+        return new Task().getInstance().setId(id);
+    }
+
+    @Override
+    protected Task getEntityInstance() {
+        return new Task().getInstance();
     }
 }
