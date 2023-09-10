b98768e3241979f0db7f35e05361416ec20de466
zay <64491586+zaytiri@users.noreply.github.com>
2023-09-10 12:47:15 +0100

    [ui] update module-info.java

--- ui/src/main/java/module-info.java
+++ ui/src/main/java/module-info.java
@@ -1,14 +1,14 @@
-module personal.zaytiri.taskerlyze.ui {
+module ui.main {
     requires javafx.controls;
     requires javafx.fxml;
     requires api.main;
     requires java.desktop;
 
-
-    opens personal.zaytiri.taskerlyze.ui to javafx.fxml;
-    exports personal.zaytiri.taskerlyze.ui;
-    exports personal.zaytiri.taskerlyze.ui.views;
     opens personal.zaytiri.taskerlyze.ui.views to javafx.fxml;
-    exports personal.zaytiri.taskerlyze.ui.controllers;
     opens personal.zaytiri.taskerlyze.ui.controllers to javafx.fxml;
+    opens personal.zaytiri.taskerlyze.ui.logic to javafx.fxml;
+
+    exports personal.zaytiri.taskerlyze.ui.views;
+    exports personal.zaytiri.taskerlyze.ui.controllers;
+    exports personal.zaytiri.taskerlyze.ui.logic;
 }
