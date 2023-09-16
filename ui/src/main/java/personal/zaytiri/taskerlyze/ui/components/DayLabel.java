0d661ac9174b7bd689e0faee46aa11ab6f4ee51c
zay <64491586+zaytiri@users.noreply.github.com>
2023-09-16 23:08:31 +0100

    [ui] add new component DayLabel;

---
+++ ui/src/main/java/personal/zaytiri/taskerlyze/ui/components/DayLabel.java
@@ -0,0 +1,64 @@
+package personal.zaytiri.taskerlyze.ui.components;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+
+public class DayLabel extends Label {
+    private final IntegerProperty month = new SimpleIntegerProperty();
+    private final IntegerProperty year = new SimpleIntegerProperty();
+    private final IntegerProperty day = new SimpleIntegerProperty();
+    @FXML
+    public Label label;
+
+    public DayLabel() {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/day-label.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
+        try {
+            loader.load();
+        } catch (IOException ex) {
+            throw new IllegalStateException("Could not load fxml file", ex);
+        }
+    }
+
+    public IntegerProperty dayProperty() {
+        return day;
+    }
+
+    public int getDay() {
+        return day.get();
+    }
+
+    public void setDay(int day) {
+        this.day.set(day);
+    }
+
+    public int getMonth() {
+        return month.get();
+    }
+
+    public void setMonth(int value) {
+        month.set(value);
+    }
+
+    public int getYear() {
+        return year.get();
+    }
+
+    public void setYear(int year) {
+        this.year.set(year);
+    }
+
+    public IntegerProperty monthProperty() {
+        return month;
+    }
+
+    public IntegerProperty yearProperty() {
+        return year;
+    }
+}
