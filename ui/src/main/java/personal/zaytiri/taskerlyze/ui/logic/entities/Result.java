2f45f0a2ce12257897c44ad3e7251750c7189952
zay <64491586+zaytiri@users.noreply.github.com>
2023-09-16 23:08:52 +0100

    [ui] add Result class to be used in Dialogs;

---
+++ ui/src/main/java/personal/zaytiri/taskerlyze/ui/logic/entities/Result.java
@@ -0,0 +1,26 @@
+package personal.zaytiri.taskerlyze.ui.logic.entities;
+
+public class Result<T> {
+    private T result;
+    private boolean status;
+
+    public Result(T result) {
+        this.result = result;
+    }
+
+    public T getResult() {
+        return result;
+    }
+
+    public void setResult(T result) {
+        this.result = result;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
