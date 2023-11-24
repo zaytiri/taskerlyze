aa8b9952a364d50346dbaf670cbee81c7bce5aea
zay <64491586+zaytiri@users.noreply.github.com>
2023-11-24 17:38:36 +0000

    [ui] add options for task context menu;
    add, remove, edit, add subtask, copy url and copy text ttle; removed button from display for adding a new subtask;

--- ui/src/main/java/personal/zaytiri/taskerlyze/ui/components/PaneTask.java
+++ ui/src/main/java/personal/zaytiri/taskerlyze/ui/components/PaneTask.java
@@ -3,11 +3,15 @@ package personal.zaytiri.taskerlyze.ui.components;
 import javafx.beans.property.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import personal.zaytiri.taskerlyze.ui.logic.Clipboard;
+import personal.zaytiri.taskerlyze.ui.logic.MenuOptions;
+import personal.zaytiri.taskerlyze.ui.logic.PopupAction;
 import personal.zaytiri.taskerlyze.ui.logic.SubTaskLoader;
-import personal.zaytiri.taskerlyze.ui.logic.entities.Result;
 import personal.zaytiri.taskerlyze.ui.logic.entities.SubTaskEntity;
 import personal.zaytiri.taskerlyze.ui.logic.entities.TaskEntity;
 
@@ -17,14 +21,13 @@ public class PaneTask extends TitledPane {
     private final IntegerProperty taskId = new SimpleIntegerProperty();
     private final StringProperty taskName = new SimpleStringProperty();
     private final BooleanProperty isTaskDone = new SimpleBooleanProperty();
+    private final MenuOptions contextMenu;
     @FXML
     private CheckBox checkBox;
     @FXML
     private Label task;
     @FXML
     private Button swapButton;
-    @FXML
-    private Button addNewSubTaskButton;
     @FXML
     private Accordion subTasksAccordion;
     @FXML
@@ -33,6 +36,8 @@ public class PaneTask extends TitledPane {
 
     public PaneTask() {
         FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/pane-task.fxml"));
+        this.contextMenu = new MenuOptions();
+
         loader.setRoot(this);
         loader.setController(this);
 
@@ -43,21 +48,6 @@ public class PaneTask extends TitledPane {
         }
     }
 
-    public void addSubTaskButtonSetOnAction() {
-        addNewSubTaskButton.setOnAction(event -> {
-            Result<TaskEntity> taskEntityResult = new Result<>(new TaskEntity());
-            DialogNewSubTask dialog = new DialogNewSubTask(taskEntityResult);
-            dialog.setTaskId(getTaskId());
-            dialog.showDialog();
-
-            if (taskEntityResult.isSuccessful()) {
-                setSubTasks();
-            }
-
-            event.consume();
-        });
-    }
-
     public int getTaskId() {
         return taskId.get();
     }
@@ -66,19 +56,32 @@ public class PaneTask extends TitledPane {
         this.taskId.set(taskId);
     }
 
+    public String getTaskName() {
+        return taskName.get();
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName.set(taskName);
+        task.setText(this.taskName.get());
+    }
+
+    public void setContextMenu(EventHandler<ActionEvent> ifSuccessful) {
+        setContextMenu(getTabContextMenu(ifSuccessful));
+    }
+
     public void setIsTaskDone(boolean isTaskDone) {
         this.isTaskDone.set(isTaskDone);
         checkBox.setSelected(this.isTaskDone.get());
     }
 
     public void setSubTasks() {
-        subTasksAccordion.getPanes().remove(1, subTasksAccordion.getPanes().size());
+        subTasksAccordion.getPanes().remove(0, subTasksAccordion.getPanes().size());
 
         subTaskLoader.setTaskId(getTaskId());
         ObservableList<SubTaskEntity> subTasks = FXCollections.observableList(subTaskLoader.load());
 
         if (subTasks.isEmpty()) {
-            subTasksAccordion.getPanes().add(1, notFoundMessage);
+            subTasksAccordion.getPanes().add(0, notFoundMessage);
         }
 
         for (SubTaskEntity st : subTasks) {
@@ -88,20 +91,57 @@ public class PaneTask extends TitledPane {
             comp.setIsTaskDone(st.isTaskDone());
             comp.setTaskId(st.getTaskId());
 
+            comp.setContextMenu(event -> setSubTasks());
+
             subTasksAccordion.getPanes().add(comp);
         }
     }
 
-    public void setTaskName(String taskName) {
-        this.taskName.set(taskName);
-        task.setText(this.taskName.get());
+    private void addAddSubtaskOptionForContextMenu(EventHandler<ActionEvent> ifSuccessful) {
+        this.contextMenu.addMenuItem("Add new sub-task", event -> PopupAction.showDialogForAddingSubTask(getTaskId(), ifSuccessful));
+    }
+
+    private void addAddTaskOptionForContextMenu(EventHandler<ActionEvent> ifSuccessful) {
+        this.contextMenu.addMenuItem("Add new task", event -> PopupAction.showDialogForAddingTask(ifSuccessful));
+    }
+
+    private void addCopyTextOptionForContextMenu() {
+        this.contextMenu.addMenuItem("Copy text", event -> Clipboard.addTo(getTaskName()));
+    }
+
+    private void addCopyUrlOptionForContextMenu() {
+        String taskUrl = new TaskEntity().get(getTaskId()).getUrl();
+        this.contextMenu.addMenuItem("Copy URL", event -> Clipboard.addTo(taskUrl));
+    }
+
+    private void addEditTaskOptionForContextMenu(EventHandler<ActionEvent> ifSuccessful) {
+        this.contextMenu.addMenuItem("Edit", event -> PopupAction.showDialogForEditingTask(getTaskId(), ifSuccessful));
+    }
+
+    private void addRemoveTaskOptionForContextMenu(EventHandler<ActionEvent> ifSuccessful) {
+        this.contextMenu.addMenuItem("Remove (no confirmation)", event -> {
+            TaskEntity task = new TaskEntity().setId(getTaskId());
+            if (task.remove()) {
+                ifSuccessful.handle(event);
+            }
+        });
+    }
+
+    private ContextMenu getTabContextMenu(EventHandler<ActionEvent> ifSuccessful) {
+        addAddTaskOptionForContextMenu(ifSuccessful);
+        addEditTaskOptionForContextMenu(ifSuccessful);
+        addRemoveTaskOptionForContextMenu(ifSuccessful);
+        addAddSubtaskOptionForContextMenu(ifSuccessful);
+        addCopyTextOptionForContextMenu();
+        addCopyUrlOptionForContextMenu();
+
+        return contextMenu.buildContextMenu();
     }
 
     @FXML
     private void initialize() {
         subTaskLoader = new SubTaskLoader();
         setCheckBoxOnAction();
-        addSubTaskButtonSetOnAction();
         populateSubTasksWhenTaskExpands();
     }
 
