f5f8dcf3993ce1716802042f4a0fb8742782fb05
zay <64491586+zaytiri@users.noreply.github.com>
2023-11-29 22:05:08 +0000

    add method to format dates into a string;

--- ui/src/main/java/personal/zaytiri/taskerlyze/ui/logic/DateConversion.java
+++ ui/src/main/java/personal/zaytiri/taskerlyze/ui/logic/DateConversion.java
@@ -8,6 +8,14 @@ public class DateConversion {
         return LocalDate.of(year, month, day);
     }
 
+    public static String formatDateWithAbbreviatedMonth(LocalDate date) {
+        return date.getDayOfMonth() +
+                "-" +
+                date.getMonth().name().substring(0, 3) +
+                "-" +
+                date.getYear();
+    }
+
     public static String getNameOfMonthCapitalized(LocalDate date) {
         String monthName = date.getMonth().name().toLowerCase();
         return monthName.substring(0, 1).toUpperCase() + monthName.substring(1);
