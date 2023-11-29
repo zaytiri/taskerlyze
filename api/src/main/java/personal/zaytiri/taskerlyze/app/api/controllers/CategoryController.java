25ef4cf140d9141cf4c2fdc9472a1c1f14ce37ed
zay <64491586+zaytiri@users.noreply.github.com>
2023-11-29 22:22:28 +0000

    add feature to archive tasks when category is removed;

--- api/src/main/java/personal/zaytiri/taskerlyze/app/api/controllers/CategoryController.java
+++ api/src/main/java/personal/zaytiri/taskerlyze/app/api/controllers/CategoryController.java
@@ -1,9 +1,44 @@
 package personal.zaytiri.taskerlyze.app.api.controllers;
 
 import personal.zaytiri.taskerlyze.app.api.controllers.base.Controller;
+import personal.zaytiri.taskerlyze.app.api.controllers.result.CodeResult;
+import personal.zaytiri.taskerlyze.app.api.controllers.result.MessageResult;
+import personal.zaytiri.taskerlyze.app.api.controllers.result.OperationResult;
 import personal.zaytiri.taskerlyze.app.api.domain.Category;
+import personal.zaytiri.taskerlyze.app.api.domain.Task;
+import personal.zaytiri.taskerlyze.libraries.pairs.Pair;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CategoryController extends Controller<Category> {
+
+    @Override
+    public OperationResult<Category> delete(int id) {
+        Category toBeDeleted = getEntityInstance(id);
+
+        boolean isDeleted = toBeDeleted.delete();
+
+        MessageResult message = new MessageResult();
+        if (isDeleted) {
+
+            Map<String, Pair<String, Object>> filters = new HashMap<>();
+            filters.put("category_id", new Pair<>("=", id));
+
+            List<Task> results = new Task().getInstance().get(filters, null);
+            for (Task task : results) {
+                task.setCategoryId(0);
+                task.update();
+            }
+
+            message.setCode(CodeResult.DELETED);
+        } else {
+            message.setCode(CodeResult.NOT_DELETED);
+        }
+
+        return new OperationResult<>(isDeleted, message, null);
+    }
 
     @Override
     protected Category getEntityInstance(int id) {
