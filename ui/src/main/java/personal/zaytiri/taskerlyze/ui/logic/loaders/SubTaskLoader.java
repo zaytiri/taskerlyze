51184ee0862e84740a6496c42acdef6de93d0d24
zay <64491586+zaytiri@users.noreply.github.com>
2023-11-29 17:42:53 +0000

    [ui] make subtask loader into singleton and setup observer

--- ui/src/main/java/personal/zaytiri/taskerlyze/ui/logic/loaders/SubTaskLoader.java
+++ ui/src/main/java/personal/zaytiri/taskerlyze/ui/logic/loaders/SubTaskLoader.java
@@ -4,12 +4,31 @@ import personal.zaytiri.taskerlyze.app.api.controllers.SubTaskController;
 import personal.zaytiri.taskerlyze.app.api.domain.SubTask;
 import personal.zaytiri.taskerlyze.ui.logic.entities.SubTaskEntity;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SubTaskLoader {
-    private List<SubTaskEntity> loadedSubTasks;
+    private static SubTaskLoader INSTANCE;
+    private final PropertyChangeSupport support;
+    private final List<SubTaskEntity> loadedSubTasks = new ArrayList<>();
     private int taskId;
+
+    private SubTaskLoader() {
+        support = new PropertyChangeSupport(this);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener pcl) {
+        support.addPropertyChangeListener(pcl);
+    }
+
+    public static SubTaskLoader getSubTaskLoader() {
+        if (INSTANCE == null) {
+            INSTANCE = new SubTaskLoader();
+        }
+        return INSTANCE;
+    }
 
     public List<SubTaskEntity> load() {
         List<SubTaskEntity> subTasksToBeReturned = new ArrayList<>();
@@ -18,7 +37,9 @@ public class SubTaskLoader {
         for (SubTask st : subTasks) {
             subTasksToBeReturned.add(new SubTaskEntity(st));
         }
-        loadedSubTasks = new ArrayList<>();
+        support.firePropertyChange("loadedSubTasks", loadedSubTasks, subTasksToBeReturned);
+
+        loadedSubTasks.clear();
         loadedSubTasks.addAll(subTasksToBeReturned);
         return loadedSubTasks;
     }
