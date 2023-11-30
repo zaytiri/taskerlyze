8761243bb4deba101bc14935acdb0f0085c2e5f3
zay <64491586+zaytiri@users.noreply.github.com>
2023-11-30 18:36:37 +0000

    [ui] fix issue where achivements not updating properly;

--- ui/src/main/java/personal/zaytiri/taskerlyze/ui/views/components/ComponentAchievements.java
+++ ui/src/main/java/personal/zaytiri/taskerlyze/ui/views/components/ComponentAchievements.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.VBox;
 import personal.zaytiri.taskerlyze.ui.logic.DateConversion;
+import personal.zaytiri.taskerlyze.ui.logic.entities.TaskEntity;
 import personal.zaytiri.taskerlyze.ui.logic.loaders.TaskLoader;
 
 import java.beans.PropertyChangeEvent;
@@ -17,6 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ComponentAchievements extends TitledPane implements PropertyChangeListener {
+    private final List<String> achievements = new ArrayList<>();
     @FXML
     private TitledPane achievementsTitledPane;
     @FXML
@@ -38,11 +40,21 @@ public class ComponentAchievements extends TitledPane implements PropertyChangeL
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        if (!(evt.getNewValue() instanceof LocalDate)) {
-            return;
+        if ((evt.getNewValue() instanceof LocalDate)) {
+            this.activeDay = (LocalDate) evt.getNewValue();
         }
 
-        this.activeDay = (LocalDate) evt.getNewValue();
+        if ((evt.getNewValue() instanceof List)) {
+            List<TaskEntity> tasks = (List<TaskEntity>) evt.getNewValue();
+            achievements.clear();
+            for (TaskEntity t : tasks) {
+                if (t.getAchieved().isEmpty()) {
+                    continue;
+                }
+                achievements.add(t.getAchieved());
+            }
+
+        }
         showAchievements();
     }
 
@@ -58,32 +70,30 @@ public class ComponentAchievements extends TitledPane implements PropertyChangeL
 
     private void showAchievements() {
         if (isToday()) {
-            showAchivementsDisplay(false);
+            showAchievementsDisplay(false);
             return;
         }
 
-        showAchivementsDisplay(true);
+        showAchievementsDisplay(true);
 
         achievementDate.setText(DateConversion.formatDateWithAbbreviatedMonth(this.activeDay));
 
-        List<String> achievements = new ArrayList<>();//TaskLoader.getTaskLoader().loadAchievements();
         List<Node> children = achievementsContent.getChildren();
 
-        children.removeAll(children);
+        children.clear();
         if (achievements.isEmpty()) {
             children.add(new Label("No achievements were set for this day."));
         }
 
-//        for (String achievement : achievements) {
-//            Label ach = new Label();
-//            ach.setText(achievement);
-//
-//            children.add(ach);
-//        }
+        for (String achievement : achievements) {
+            Label ach = new Label();
+            ach.setText(achievement);
+            children.add(ach);
+        }
 
     }
 
-    private void showAchivementsDisplay(boolean show) {
+    private void showAchievementsDisplay(boolean show) {
         achievementsTitledPane.setVisible(show);
         achievementsTitledPane.setManaged(show);
         achievementsTitledPane.setDisable(!show);
