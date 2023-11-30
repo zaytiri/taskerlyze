b5daa3c3af391fcb246ebe86283b92c504de33b8
zay <64491586+zaytiri@users.noreply.github.com>
2023-11-30 19:56:08 +0000

    [ui] update category loader as singleton;

--- ui/src/main/java/personal/zaytiri/taskerlyze/ui/logic/loaders/CategoryLoader.java
+++ ui/src/main/java/personal/zaytiri/taskerlyze/ui/logic/loaders/CategoryLoader.java
@@ -4,10 +4,30 @@ import personal.zaytiri.taskerlyze.app.api.controllers.CategoryController;
 import personal.zaytiri.taskerlyze.app.api.domain.Category;
 import personal.zaytiri.taskerlyze.ui.logic.entities.CategoryEntity;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CategoryLoader {
+    private static CategoryLoader INSTANCE;
+    private final PropertyChangeSupport support;
+    private final List<CategoryEntity> loadedCategories = new ArrayList<>();
+
+    private CategoryLoader() {
+        support = new PropertyChangeSupport(this);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener pcl) {
+        support.addPropertyChangeListener(pcl);
+    }
+
+    public static CategoryLoader getCategoryLoader() {
+        if (INSTANCE == null) {
+            INSTANCE = new CategoryLoader();
+        }
+        return INSTANCE;
+    }
 
     public List<CategoryEntity> load() {
         List<CategoryEntity> categoriesToBeReturned = new ArrayList<>();
@@ -17,7 +37,14 @@ public class CategoryLoader {
         for (Category cat : categories) {
             categoriesToBeReturned.add(new CategoryEntity(cat));
         }
+        support.firePropertyChange("loadedCategories", loadedCategories, categoriesToBeReturned);
 
+        loadedCategories.clear();
+        loadedCategories.addAll(categoriesToBeReturned);
         return categoriesToBeReturned;
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener pcl) {
+        support.removePropertyChangeListener(pcl);
     }
 }
