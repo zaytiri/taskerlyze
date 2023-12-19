8c9c25e12b1c919b68c385b8486c7b4455c3f292
zay <64491586+zaytiri@users.noreply.github.com>
2023-12-19 20:25:15 +0000

    add abstracts set id and name required for all child classes of entity

--- api/src/main/java/personal/zaytiri/taskerlyze/app/api/domain/base/Entity.java
+++ api/src/main/java/personal/zaytiri/taskerlyze/app/api/domain/base/Entity.java
@@ -3,7 +3,7 @@ package personal.zaytiri.taskerlyze.app.api.domain.base;
 import personal.zaytiri.taskerlyze.app.dependencyinjection.AppComponent;
 import personal.zaytiri.taskerlyze.app.dependencyinjection.DaggerAppComponent;
 
-public abstract class Entity<E, R, M> implements IDependencyInjection<E>{
+public abstract class Entity<E, R, M> implements IDependencyInjection<E> {
     protected R repository;
     protected M mapper;
 
@@ -14,15 +14,19 @@ public abstract class Entity<E, R, M> implements IDependencyInjection<E>{
         return id;
     }
 
-    public String getName() {
-        return name;
-    }
-
     public E getInstance() {
         AppComponent component = DaggerAppComponent.create();
 
         return getInjectedComponent(component);
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract E setId(int id);
+
+    public abstract E setName(String name);
 
     protected abstract E getInjectedComponent(AppComponent component);
 }
