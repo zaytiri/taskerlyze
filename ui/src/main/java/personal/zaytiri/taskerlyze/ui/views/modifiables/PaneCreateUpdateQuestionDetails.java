721c79acccfea69159d68e956fabab57c90f8a04
zay <64491586+zaytiri@users.noreply.github.com>
2024-01-06 23:47:56 +0000

    [ui] add dialog to edit a question details info;

---
+++ ui/src/main/java/personal/zaytiri/taskerlyze/ui/views/modifiables/PaneCreateUpdateQuestionDetails.java
@@ -0,0 +1,46 @@
+package personal.zaytiri.taskerlyze.ui.views.modifiables;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import personal.zaytiri.taskerlyze.ui.logic.DateConversion;
+import personal.zaytiri.taskerlyze.ui.logic.entities.QuestionEntity;
+
+import java.io.IOException;
+
+public class PaneCreateUpdateQuestionDetails extends BorderPane {
+    private int questionId;
+    @FXML
+    private TextField answer;
+    @FXML
+    private Label answeredAt;
+    private QuestionEntity question;
+
+    public PaneCreateUpdateQuestionDetails() {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/pane-create-update-question-details.fxml"));
+
+        loader.setRoot(this);
+        loader.setController(this);
+
+        try {
+            loader.load();
+        } catch (IOException ex) {
+            throw new IllegalStateException("Could not load fxml file", ex);
+        }
+    }
+
+    public QuestionEntity getQuestion() {
+        return question.setAnswer(answer.getText());
+    }
+
+    public void setQuestion(QuestionEntity question) {
+        this.question = question;
+    }
+
+    public void load() {
+        answer.setText(question.getAnswer());
+        answeredAt.setText(DateConversion.getFormattedDateForUi(question.getAnsweredAt()));
+    }
+}
