c1c01f04a4c27d9bb005ae025b5208389fc083b2
zay <64491586+zaytiri@users.noreply.github.com>
2024-01-06 23:46:40 +0000

    [ui] add new pane questions details;

---
+++ ui/src/main/java/personal/zaytiri/taskerlyze/ui/views/elements/PaneQuestionDetails.java
@@ -0,0 +1,44 @@
+package personal.zaytiri.taskerlyze.ui.views.elements;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import personal.zaytiri.taskerlyze.ui.logic.DateConversion;
+import personal.zaytiri.taskerlyze.ui.logic.entities.QuestionEntity;
+
+import java.io.IOException;
+
+public class PaneQuestionDetails extends BorderPane {
+    private int questionId;
+    @FXML
+    private Label answer;
+    @FXML
+    private Label answeredAt;
+
+    public PaneQuestionDetails() {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/pane-question-details.fxml"));
+
+        loader.setRoot(this);
+        loader.setController(this);
+
+        try {
+            loader.load();
+        } catch (IOException ex) {
+            throw new IllegalStateException("Could not load fxml file", ex);
+        }
+    }
+
+    public void load() {
+        QuestionEntity question = new QuestionEntity(questionId).get();
+
+        answer.setText(question.getAnswer());
+        answeredAt.setText(DateConversion.getFormattedDateForUi(question.getAnsweredAt()));
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+
+}
