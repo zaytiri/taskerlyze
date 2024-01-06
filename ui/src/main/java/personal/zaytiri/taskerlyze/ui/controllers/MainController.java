79b8b11ca3290232f51822d38cd074bb8a3d0a28
zay <64491586+zaytiri@users.noreply.github.com>
2024-01-06 19:56:39 +0000

    [ui] remove side menu to be replaced

--- ui/src/main/java/personal/zaytiri/taskerlyze/ui/controllers/MainController.java
+++ ui/src/main/java/personal/zaytiri/taskerlyze/ui/controllers/MainController.java
@@ -1,17 +1,7 @@
 package personal.zaytiri.taskerlyze.ui.controllers;
 
-import javafx.fxml.FXML;
-import personal.zaytiri.taskerlyze.ui.views.components.ComponentCalendar;
-import personal.zaytiri.taskerlyze.ui.views.components.ComponentSideMenu;
-
-
 public class MainController {
-    @FXML
-    private ComponentSideMenu componentSideMenu;
-    @FXML
-    private ComponentCalendar componentCalendar;
 
     public void setAppPreConfigurations() {
-        componentSideMenu.addPropertyChangeListener(componentCalendar);
     }
 }
