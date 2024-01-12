60d4e24121e4a1b44ccef88d303163ac3ec44174
zay <64491586+zaytiri@users.noreply.github.com>
2024-01-12 19:50:34 +0000

    [ui] deep refactoring for task loader;
    removed caching from task loader side; make task loader not a singleton; only use task loader for loading tasks from the api;

--- ui/src/main/java/personal/zaytiri/taskerlyze/ui/logic/loaders/TaskLoader.java
+++ ui/src/main/java/personal/zaytiri/taskerlyze/ui/logic/loaders/TaskLoader.java
@@ -1,126 +1,25 @@
 package personal.zaytiri.taskerlyze.ui.logic.loaders;
 
-import com.google.common.base.Stopwatch;
 import personal.zaytiri.taskerlyze.app.api.controllers.TaskController;
 import personal.zaytiri.taskerlyze.app.api.controllers.result.OperationResult;
 import personal.zaytiri.taskerlyze.app.api.domain.Task;
-import personal.zaytiri.taskerlyze.libraries.pairs.Pair;
 import personal.zaytiri.taskerlyze.ui.logic.entities.TaskEntity;
-import personal.zaytiri.taskerlyze.ui.logic.uifuncionality.Cache;
 
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class TaskLoader {
-    private static TaskLoader INSTANCE;
-    private final PropertyChangeSupport support;
-    private final List<TaskEntity> loadedTasks = new ArrayList<>();
 
-    private final Cache cache = new Cache();
-    private LocalDate activeDay = null;
-    private int activeCategoryId = 0;
-
-    private TaskLoader() {
-        support = new PropertyChangeSupport(this);
-    }
-
-    public void addPropertyChangeListener(PropertyChangeListener pcl) {
-        support.addPropertyChangeListener(pcl);
-    }
-
-    public int getActiveCategoryId() {
-        return activeCategoryId;
-    }
-
-    public void setActiveCategoryId(int currentActiveCategoryId) {
-        activeCategoryId = currentActiveCategoryId;
-        load();
-    }
-
-    public LocalDate getActiveDay() {
-        return activeDay;
-    }
-
-    public void setActiveDay(LocalDate currentActiveDay) {
-        support.firePropertyChange("activeDay", activeDay, currentActiveDay);
-        activeDay = currentActiveDay;
-        load();
-    }
-
-    public static TaskLoader getTaskLoader() {
-        if (INSTANCE == null) {
-            INSTANCE = new TaskLoader();
-        }
-        return INSTANCE;
-    }
-
-    public void load() {
-        if (activeDay == null) {
-            return;
-        }
-        
-        List<TaskEntity> tasksToBeReturned = loadFromApiOrCache();
-
-        Stopwatch loadTasksFromUiSw = Stopwatch.createStarted();
-        support.firePropertyChange("loadedTasks", loadedTasks, tasksToBeReturned);
-
-        loadedTasks.clear();
-        loadedTasks.addAll(tasksToBeReturned);
-
-        loadTasksFromUiSw.stop();
-        System.out.println("loadTasksFromUiSw took " + loadTasksFromUiSw.elapsed(TimeUnit.MILLISECONDS) + " ms");
-    }
-
-    public void refresh() {
-        setCacheReloadFor(activeCategoryId, activeDay, true);
-        load();
-    }
-
-    public void removePropertyChangeListener(PropertyChangeListener pcl) {
-        support.removePropertyChangeListener(pcl);
-    }
-
-    public void setCacheReloadFor(int categoryId, LocalDate date, boolean toBeReloaded) {
-        var currentCache = cache.findByKey(new Pair<>(categoryId, date));
-        cache.replace(currentCache.getKey(), new Pair<>(toBeReloaded, currentCache.getValue().getValue()));
-    }
-
-    private List<TaskEntity> loadFromApiOrCache() {
-        Stopwatch loadTasksFromApiSw = Stopwatch.createStarted();
-        Stopwatch loadTasksFromCacheSw = Stopwatch.createStarted();
-
+    public List<TaskEntity> load(int categoryId, LocalDate date) {
         List<TaskEntity> tasksToBeReturned = new ArrayList<>();
-        var cacheKey = new Pair<>(activeCategoryId, activeDay);
-        var currentCache = cache.findByKey(cacheKey);
 
-        if (currentCache != null) {
-
-            if (Boolean.FALSE.equals(currentCache.getValue().getKey())) {
-                tasksToBeReturned.addAll(currentCache.getValue().getValue());
-
-                cache.replace(cacheKey, new Pair<>(false, tasksToBeReturned));
-                loadTasksFromCacheSw.stop();
-                System.out.println("loadTasksFromCacheSw took " + loadTasksFromCacheSw.elapsed(TimeUnit.MILLISECONDS) + " ms");
-                return tasksToBeReturned;
-            }
-        }
-
-        OperationResult<List<Task>> taskResult = new TaskController().getTasksByCategoryAndCompletedAtDate(activeCategoryId, activeDay);
+        OperationResult<List<Task>> taskResult = new TaskController().getTasksByCategoryAndCompletedAtDate(categoryId, date);
         for (Task task : taskResult.getResult()) {
             tasksToBeReturned.add(new TaskEntity(task));
         }
 
-        if (currentCache != null) {
-            cache.replace(cacheKey, new Pair<>(false, tasksToBeReturned));
-        } else {
-            cache.put(cacheKey, new Pair<>(false, tasksToBeReturned));
-        }
-        loadTasksFromApiSw.stop();
-        System.out.println("loadTasksFromApiSw took " + loadTasksFromApiSw.elapsed(TimeUnit.MILLISECONDS) + " ms");
         return tasksToBeReturned;
     }
+
 }
