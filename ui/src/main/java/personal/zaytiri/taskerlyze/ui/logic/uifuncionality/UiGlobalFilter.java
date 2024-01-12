7d838a262d510843800bf3fc1bcca4030003f438
zay <64491586+zaytiri@users.noreply.github.com>
2024-01-12 19:41:55 +0000

    [ui] add a singleton class only for ui filters

---
+++ ui/src/main/java/personal/zaytiri/taskerlyze/ui/logic/uifuncionality/UiGlobalFilter.java
@@ -0,0 +1,45 @@
+package personal.zaytiri.taskerlyze.ui.logic.uifuncionality;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.time.LocalDate;
+
+public class UiGlobalFilter {
+    private static UiGlobalFilter INSTANCE;
+    private final PropertyChangeSupport support;
+    private LocalDate activeDay = null;
+    private int activeCategoryId = 0;
+
+    private UiGlobalFilter() {
+        support = new PropertyChangeSupport(this);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener pcl) {
+        support.addPropertyChangeListener(pcl);
+    }
+
+    public int getActiveCategoryId() {
+        return activeCategoryId;
+    }
+
+    public void setActiveCategoryId(int currentActiveCategoryId) {
+        activeCategoryId = currentActiveCategoryId;
+//        support.firePropertyChange("toReload", true, false);
+    }
+
+    public LocalDate getActiveDay() {
+        return activeDay;
+    }
+
+    public void setActiveDay(LocalDate currentActiveDay) {
+        activeDay = currentActiveDay;
+        support.firePropertyChange("toReload", true, false);
+    }
+
+    public static UiGlobalFilter getUiGlobalFilter() {
+        if (INSTANCE == null) {
+            INSTANCE = new UiGlobalFilter();
+        }
+        return INSTANCE;
+    }
+}
