c6b04b2ac6414053be39b27038a2a0d2411b9c0f
zay <64491586+zaytiri@users.noreply.github.com>
2024-01-15 16:00:32 +0000

    [ui] add generic dialog for confirmation cases;

---
+++ ui/src/main/java/personal/zaytiri/taskerlyze/ui/views/popups/DialogConfirmation.java
@@ -0,0 +1,46 @@
+package personal.zaytiri.taskerlyze.ui.views.popups;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import personal.zaytiri.taskerlyze.ui.views.popups.interfaces.Dialog;
+
+
+public class DialogConfirmation extends Dialog<Boolean> {
+    private EventHandler<ActionEvent> afterSuccessful;
+    @FXML
+    private Label dialogContent;
+    @FXML
+    private Button buttonYes;
+    @FXML
+    private Button buttonNo;
+
+    public DialogConfirmation() {
+        super("dialog-confirmation", "Are you sure?");
+    }
+
+    @FXML
+    public void initialize() {
+        buttonNo.setOnAction(e -> closeDialog());
+    }
+
+    public void setAfterSuccessful(EventHandler<ActionEvent> afterSuccessful) {
+        this.afterSuccessful = afterSuccessful;
+    }
+
+    public void setMessage(String message) {
+        dialogContent.setText(message);
+    }
+
+    @Override
+    public void showDialog() {
+        buttonYes.setOnAction(e -> {
+            this.afterSuccessful.handle(new ActionEvent());
+            closeDialog();
+        });
+
+        show();
+    }
+}
