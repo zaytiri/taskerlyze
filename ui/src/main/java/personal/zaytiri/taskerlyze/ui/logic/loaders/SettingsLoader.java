9f0d2f3f806ee98410dafa0b828901463535afbc
zay <64491586+zaytiri@users.noreply.github.com>
2024-01-22 16:38:23 +0000

    [ui] refacto settings loader using loadable;

--- ui/src/main/java/personal/zaytiri/taskerlyze/ui/logic/loaders/SettingsLoader.java
+++ ui/src/main/java/personal/zaytiri/taskerlyze/ui/logic/loaders/SettingsLoader.java
@@ -5,41 +5,28 @@ import personal.zaytiri.taskerlyze.app.api.domain.Settings;
 import personal.zaytiri.taskerlyze.ui.logic.entities.SettingsEntity;
 import personal.zaytiri.taskerlyze.ui.logic.mappers.SettingsMapper;
 
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SettingsLoader {
-    private static SettingsLoader INSTANCE;
-    private final PropertyChangeSupport support;
-    private SettingsEntity loadedSettings = new SettingsEntity();
+public class SettingsLoader implements Loadable<SettingsEntity> {
+    private int settingsId;
 
-    private SettingsLoader() {
-        support = new PropertyChangeSupport(this);
-    }
-
-    public void addPropertyChangeListener(PropertyChangeListener pcl) {
-        support.addPropertyChangeListener(pcl);
-    }
-
-    public static SettingsLoader getSettingsLoader() {
-        if (INSTANCE == null) {
-            INSTANCE = new SettingsLoader();
-        }
-        return INSTANCE;
-    }
-
-    public SettingsEntity load() {
+    @Override
+    public List<SettingsEntity> load() {
         SettingsEntity settingsToBeReturned = new SettingsEntity();
 
-        Settings settings = new SettingsController().get(0).getResult();
+        Settings settings = new SettingsController().get(settingsId).getResult();
 
-        settingsToBeReturned = new SettingsMapper().mapToUiObject(settings, settingsToBeReturned);
+        List<SettingsEntity> allSettings = new ArrayList<>();
 
-        support.firePropertyChange("loadedSettings", loadedSettings, settingsToBeReturned);
+        if (settings != null) {
+            allSettings.add(new SettingsMapper().mapToUiObject(settings, settingsToBeReturned));
+        }
 
-        loadedSettings = settingsToBeReturned;
-
-        return loadedSettings;
+        return allSettings;
     }
 
+    public void setSettingsId(int settingsId) {
+        this.settingsId = settingsId;
+    }
 }
