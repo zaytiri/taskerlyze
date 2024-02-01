2a5784d7abbece9583ed520d91ed3f0abcee9e0b
zay <64491586+zaytiri@users.noreply.github.com>
2024-02-01 22:25:18 +0000

    Update module-info.java

--- ui/src/main/java/module-info.java
+++ ui/src/main/java/module-info.java
@@ -8,12 +8,10 @@ module ui.main {
 
     opens personal.zaytiri.taskerlyze.ui.views to javafx.fxml;
     opens personal.zaytiri.taskerlyze.ui.controllers to javafx.fxml;
-    opens personal.zaytiri.taskerlyze.ui.logic to javafx.fxml;
     opens personal.zaytiri.taskerlyze.ui.views.components to javafx.fxml;
 
     exports personal.zaytiri.taskerlyze.ui.views;
     exports personal.zaytiri.taskerlyze.ui.controllers;
-    exports personal.zaytiri.taskerlyze.ui.logic;
     exports personal.zaytiri.taskerlyze.ui.views.components;
     exports personal.zaytiri.taskerlyze.ui.logic.entities;
     opens personal.zaytiri.taskerlyze.ui.logic.entities to javafx.fxml;
